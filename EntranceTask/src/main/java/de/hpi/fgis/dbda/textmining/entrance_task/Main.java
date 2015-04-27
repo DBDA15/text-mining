6daa8eb341e06222b89f0358f2d0da986d7f8b36
David Heller <daheller@ucsc.edu>
2015-04-27 16:08:30 +0200

    made tagger transient switched to a faster tagger added short input file

--- EntranceTask/src/main/java/de/hpi/fgis/dbda/textmining/entrance_task/Main.java
+++ EntranceTask/src/main/java/de/hpi/fgis/dbda/textmining/entrance_task/Main.java
@@ -2,59 +2,54 @@ package de.hpi.fgis.dbda.textmining.entrance_task;
 
 import java.io.Serializable;
 
-import edu.stanford.nlp.tagger.maxent.MaxentTagger;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.Function;
 
-import java.util.*;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
-/**
- * Hello world!
- *
- */
-public class Main 
+public class Main
 {
+    static transient MaxentTagger tagger = null;
+
     public static void main( String[] args )
     {
-    	// String tagged = tagger.tagString(sample);
+
+        // String tagged = tagger.tagString(sample);
 
         // initialize spark environment
         SparkConf config = new SparkConf().setAppName(Main.class.getName());
         config.set("spark.hadoop.validateOutputSpecs", "false");
 
         try(JavaSparkContext context = new JavaSparkContext(config)) {
-        	 JavaRDD<String> lineItems = context
-        	    .textFile("data/nyt.1987.tsv")
-	        	.mapPartitionsWithIndex(
-                        new Function2<Integer, Iterator<String>, Iterator<String>>() {
-                            public Iterator<String> call(Integer i, Iterator<String> iter) {
-                                final MaxentTagger tagger = new MaxentTagger("pos_tagger/taggers/english-bidirectional-distsim.tagger");
-                                List res = new ArrayList();
-                                while (iter.hasNext()) {
-                                    String str = iter.next();
-                                    LineItem li = new LineItem(str);
+            JavaRDD<String> lineItems = context
+                    .textFile("data/short.tsv")
+                    .map(
+                            new Function<String, String>() {
+                                public String call(String line) {
+                                    if(tagger== null) {
+                                        tagger = new MaxentTagger("pos_tagger/taggers/english-left3words-distsim.tagger");
+                                    }
+                                    LineItem li = new LineItem(line);
                                     String tagged = tagger.tagString(li.TEXT);
-                                    res.add(tagged);
+                                    //System.out.println(tagged);
+                                    return tagged;
                                 }
-                                return res.iterator();
-                            }
-                        }, true);
-	            
-	            lineItems.saveAsTextFile("data/test");
+                            });
+
+            lineItems.saveAsTextFile("data/test");
         }
     }
-    
+
     static class LineItem implements Serializable {
         Integer INDEX;
         String TEXT;
 
         LineItem(String line) {
-          String[] values = line.split("\t");
-          INDEX = Integer.parseInt(values[0]);
-          TEXT = values[1];
+            String[] values = line.split("\t");
+            INDEX = Integer.parseInt(values[0]);
+            TEXT = values[4];
         }
-      }
+    }
 }

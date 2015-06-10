ad41a3acf485df47975dcff81fdff5f8714e00b3
Bastien Grasnick <b.grasnick@gmail.com>
2015-06-10 11:16:05 +0200

    set up flink project

---
+++ MainTask-flink/MainTask-flink/src/main/java/de/hpi/fgis/dbda/textmining/MainTask_flink/App.java
@@ -0,0 +1,13 @@
+package de.hpi.fgis.dbda.textmining.MainTask_flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
